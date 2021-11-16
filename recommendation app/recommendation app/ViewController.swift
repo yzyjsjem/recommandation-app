2605aa3ea745a7b4fffcba5af3e6b0643098bb37
Zhenyu Yang <50858833+yzyjsjem@users.noreply.github.com>
2021-11-16 17:40:47 +1100

    finish the basic three page and navigation bar
    find a bug that never show before:
    failed to initialize the first view
    solution: choose the initial view controller tag. Don't know why it never happened before

---
+++ recommendation app/recommendation app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recommendation app
+//
+//  Created by 杨振誉 on 2021/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ recommendation app/recommendation app/restaurant.swift
@@ -0,0 +1,29 @@
+//
+//  restaurant.swift
+//  recommendation app
+//
+//  Created by 杨振誉 on 2021/11/16.
+//
+
+import UIKit
+
+class restaurant: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
